f378ad58df40fb99531d94b8052eb52b596074ed
DecentralizedCAN <DecentralizedCAN@gmail.com>
2019-09-25 11:30:36 -0400

    remove quote finder

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -20,8 +20,6 @@ class StaticController < ApplicationController
     @current_time = Time.now.to_i
     @notifications = current_user.notification.order("created_at DESC").first(6) if logged_in?
 
-    find_quote
-
 	end
 
 	def documentation
@@ -68,48 +66,4 @@ class StaticController < ApplicationController
 		render "wakemydyno.txt"
 	end
 
-	def find_quote
-		@quotes = [
-		    ['I\'ve said it since day one: I can\'t do it alone.', 'Bernie Sanders'],
-		    ['The only way that real change takes place is when millions of people stand up, fight back, and say, "Enough is enough. We\'re gonna have a government that works for all of us, not just the few."', 'Bernie Sanders'],
-		    ['Some individuals and groups...may naïvely think that if they simply espouse their goal strongly, firmly, and long enough, it will somehow come to pass...The espousal of humane goals and loyalty to ideals are admirable, but are grossly inadequate.', 'Gene Sharp'],
-		    ['Real change never takes place from the top on down. It always takes place from the bottom on up. It takes place when ordinary people, by the millions, are prepared to stand up and fight for justice.', 'Bernie Sanders'],
-		    [' It is not just about electing Bernie Sanders for president, it is about creating a grassroots political movement in this country.', 'Bernie Sanders'],
-		    ['Let us wage a moral and political war against the billionaires and corporate leaders, on Wall Street and elsewhere, whose policies and greed are destroying the middle class of America.', 'Bernie Sanders'],
-		    ['Rise like Lions after slumber, in unvanquishable number. Shake your chains to earth like dew, which in sleep had fallen on you. 
-		      Ye are many — they are few.', 'Percy Bysshe Shelley'],
-		    ['We need a mass movement of people over many years to bring about the change that we need. That is what this political revolution is about—returning power to the people, where it belongs.', 'Bernie Sanders'],
-		    ['When millions of people stand up and fight...they win.', 'Bernie Sanders'],
-		    ['All your political agitation will end as other agitations have done, in smoke, unless you band yourselves together...and organize.', 'Charles Stewart Parnell'],
-		    ['the only way we will pass policies like Medicare for All and a Green New Deal is by organizing the largest grassroots movement American politics has ever seen.', 'Bernie Sanders'],
-		    # [],
-		    # [],
-		    # [],
-		    ['I\'ve said it since day one: I can\'t do it alone.', 'Bernie Sanders'],
-		    ['The only way that real change takes place is when millions of people stand up, fight back, and say, "Enough is enough. We\'re gonna have a government that works for all of us, not just the few."', 'Bernie Sanders'],
-		    ['Some individuals and groups...may naïvely think that if they simply espouse their goal strongly, firmly, and long enough, it will somehow come to pass...The espousal of humane goals and loyalty to ideals are admirable, but are grossly inadequate.', 'Gene Sharp'],
-		    ['Real change never takes place from the top on down. It always takes place from the bottom on up. It takes place when ordinary people, by the millions, are prepared to stand up and fight for justice.', 'Bernie Sanders'],
-		    [' It is not just about electing Bernie Sanders for president, it is about creating a grassroots political movement in this country.', 'Bernie Sanders'],
-		    ['Let us wage a moral and political war against the billionaires and corporate leaders, on Wall Street and elsewhere, whose policies and greed are destroying the middle class of America.', 'Bernie Sanders'],
-		    ['Rise like Lions after slumber, in unvanquishable number. Shake your chains to earth like dew, which in sleep had fallen on you. 
-		      Ye are many — they are few.', 'Percy Bysshe Shelley'],
-		    ['We need a mass movement of people over many years to bring about the change that we need. That is what this political revolution is about—returning power to the people, where it belongs.', 'Bernie Sanders'],
-		    ['When millions of people stand up and fight...they win.', 'Bernie Sanders'],
-		    ['All your political agitation will end as other agitations have done, in smoke, unless you band yourselves together...and organize.', 'Charles Stewart Parnell'],
-		    ['I\'ve said it since day one: I can\'t do it alone.', 'Bernie Sanders'],
-		    ['The only way that real change takes place is when millions of people stand up, fight back, and say, "Enough is enough. We\'re gonna have a government that works for all of us, not just the few."', 'Bernie Sanders'],
-		    ['Some individuals and groups...may naïvely think that if they simply espouse their goal strongly, firmly, and long enough, it will somehow come to pass...The espousal of humane goals and loyalty to ideals are admirable, but are grossly inadequate.', 'Gene Sharp'],
-		    ['Real change never takes place from the top on down. It always takes place from the bottom on up. It takes place when ordinary people, by the millions, are prepared to stand up and fight for justice.', 'Bernie Sanders'],
-		    [' It is not just about electing Bernie Sanders for president, it is about creating a grassroots political movement in this country.', 'Bernie Sanders'],
-		    ['Let us wage a moral and political war against the billionaires and corporate leaders, on Wall Street and elsewhere, whose policies and greed are destroying the middle class of America.', 'Bernie Sanders'],
-		    ['Rise like Lions after slumber, in unvanquishable number. Shake your chains to earth like dew, which in sleep had fallen on you. 
-		      Ye are many — they are few.', 'Percy Bysshe Shelley'],
-		    ['We need a mass movement of people over many years to bring about the change that we need. That is what this political revolution is about—returning power to the people, where it belongs.', 'Bernie Sanders'],
-		    ['When millions of people stand up and fight...they win.', 'Bernie Sanders'],
-		    ['All your political agitation will end as other agitations have done, in smoke, unless you band yourselves together...and organize.', 'Charles Stewart Parnell'],
-
-		  ]
-
-		  @quote = @quotes[Time.now.hour]
-	end
 end
