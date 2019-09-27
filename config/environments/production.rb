a4979d48849c00635279bc63993f9912cef21d86
DecentralizedCAN <DecentralizedCAN@gmail.com>
2019-09-27 10:23:27 -0400

    fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => ENV['MAILER_DOMAIN'],
     :enable_starttls_auto => true
   }
 
