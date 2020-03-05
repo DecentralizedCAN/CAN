50f2603060220391bc27aa5eeb93cb4476c8fe66
DecentralizedCAN <DecentralizedCAN@gmail.com>
2020-03-04 23:57:36 -0500

    troubleshooting no link creation in heroku

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,5 @@
 class Link < ApplicationRecord
-	belongs_to :parent, class_name: 'Goal'
-	belongs_to :child, class_name: 'Goal'
+	belongs_to :parent, class_name: 'Goal', optional: true
+	belongs_to :child, class_name: 'Goal', optional: true
 	has_and_belongs_to_many :user
 end
