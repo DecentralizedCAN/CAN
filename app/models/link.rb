5632c4b75a1bb418f788b267b223e8a1de1b1357
DecentralizedCAN <DecentralizedCAN@gmail.com>
2020-03-05 00:39:52 -0500

    troubleshooting link saving in heroku

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,7 @@
 class Link < ApplicationRecord
 	belongs_to :parent, class_name: 'Goal'
 	belongs_to :child, class_name: 'Goal'
-  belongs_to :goal, optional: true
-  belongs_to :activity, optional: true
+  validates :goal, presence: false
+  validates :activities, presence: false
 	has_and_belongs_to_many :user
 end
