0926a4d636bb3942dbb0a1bd4495d72f06bec1b2
DecentralizedCAN <DecentralizedCAN@gmail.com>
2020-03-29 18:46:34 -0400

    check for email length before sending

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -6,7 +6,7 @@ class NotificationMailer < ApplicationMailer
 
     	ENV['GROUP_NAME'] != nil ? group_name = ENV['GROUP_NAME'] : group_name = "Decentralized CAN"
 
-      if @notification.user.email_notifications && @notification.user.email && Setting.find(8).state
+      if @notification.user.email_notifications && @notification.user.email && @notification.user.email.length > 0 && Setting.find(8).state
   	    mail(
   	      to: @notification.user.email, 
   	      subject: "Notification from #{group_name}"
