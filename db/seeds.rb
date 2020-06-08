85684f209f951dd4b01596e33c158dacd5fdb834
DecentralizedCAN <DecentralizedCAN@gmail.com>
2020-06-07 20:51:25 -0400

    add ids to settings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-Setting.create!(title: "Allow any member to post", state: false)
-Setting.create!(title: "Limit multiple posts within a short amount of time", state: false)
-Setting.create!(title: "Allow users to sign up on their own", state: false)
-Setting.create!(title: "Include a discussion section for brainstorms", state: false)
-Setting.create!(title: "Include a discussion section for actions", state: false)
-Setting.create!(title: "Make content viewable without an account", state: false)
-Setting.create!(title: "Require new members to provide an email address", state: false)
-Setting.create!(title: "Allow users to receive emails (reduced security)", state: false)
-Setting.create!(title: "Hide proposal participation and default to 1", state: false)
-Setting.create!(title: "Allow facilitator mode", state: false)
-Setting.create!(title: "Allow multiple scoring methods", state: false)
-Setting.create!(title: "Allow anyone to broadcast new posts", state: false)
-Setting.create!(title: "Allow new goals to be posted to the feed", state: false)
+Setting.create!(id: 1, title: "Allow any member to post", state: false)
+Setting.create!(id: 2, title: "Limit multiple posts within a short amount of time", state: false)
+Setting.create!(id: 3, title: "Allow users to sign up on their own", state: false)
+Setting.create!(id: 4, title: "Include a discussion section for brainstorms", state: false)
+Setting.create!(id: 5, title: "Include a discussion section for actions", state: false)
+Setting.create!(id: 6, title: "Make content viewable without an account", state: false)
+Setting.create!(id: 7, title: "Require new members to provide an email address", state: false)
+Setting.create!(id: 8, title: "Allow users to receive emails (reduced security)", state: false)
+Setting.create!(id: 9, title: "Hide proposal participation and default to 1", state: false)
+Setting.create!(id: 10, title: "Allow facilitator mode", state: false)
+Setting.create!(id: 11, title: "Allow multiple scoring methods", state: false)
+Setting.create!(id: 12, title: "Allow anyone to broadcast new posts", state: false)
+Setting.create!(id: 13, title: "Allow new goals to be posted to the feed", state: false)
 
 ENV['ADMIN_EMAIL'] != nil ? admin_email = ENV['ADMIN_EMAIL'] : admin_email = "admin@fakeemail.com"
 ENV['ADMIN_PASSWORD'] != nil ? admin_password = ENV['ADMIN_PASSWORD'] : admin_password = "adminpassword"
