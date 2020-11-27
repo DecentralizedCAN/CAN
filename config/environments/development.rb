825a0094ed6e1285e50cab0e3cabb510fb0d4848
DecentralizedCAN <DecentralizedCAN@gmail.com>
2020-11-27 16:37:12 -0500

    new designs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # ENV['GROUP_NAME'] = 'Xymmer'
-  ENV['GROUP_NAME'] = "Odin Days"
+  # ENV['GROUP_NAME'] = 'DCAN'
   ENV['LOCKBOX_MASTER_KEY'] = '0000000000000000000000000000000000000000000000000000000000000000'
   ENV['BLIND_INDEX_MASTER_KEY']='ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
   ENV['SERVER_HOST']='localhost:3000'
