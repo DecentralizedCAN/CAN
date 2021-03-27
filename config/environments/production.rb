6104a02fcdc21cf89b9f0a1cf2e330f2e19d8bb1
DecentralizedCAN <DecentralizedCAN@gmail.com>
2021-03-26 21:10:57 -0400

    troubleshooting emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
+    :domain               => 'mail.google.com',
     :port                 => 587,
     :user_name            => ENV['GMAIL_USERNAME'],
     :password             => ENV['GMAIL_PASSWORD'],
