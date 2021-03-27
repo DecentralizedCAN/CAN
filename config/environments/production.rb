c8b75965974ee26c47181ea9ee2126091e94e005
DecentralizedCAN <DecentralizedCAN@gmail.com>
2021-03-26 21:22:35 -0400

    add http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = ENV['SERVER_HOST']
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
 
   # ActionMailer::Base.smtp_settings = {
